4d0135430887423030555a96cbd03007d1429346
Saori Miyamoto <s.miyamoto1509@gmail.com>
2017-12-14 16:01:09 +0000

    login n post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  resources :tasks
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
+  resources :tasklists, only: [:create, :destroy]
 end
